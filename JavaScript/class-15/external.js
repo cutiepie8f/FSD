3b71dbae41506c11116493a1e8b45190ff70cd54
cutiepie8f <135813683+cutiepie8f@users.noreply.github.com>
2024-01-22 00:32:35 +0530

    today class

---
+++ JavaScript/class-15/external.js
@@ -0,0 +1,9 @@
+        var p = 20;
+         var q = 30;
+         var res = subtraction(x,y);  // x,y are the arguments.
+        document.write("sub is : "+res+"<br>")
+         function subtraction(a,b)       // a and b are the parameters.
+         {
+            var sub = b - a;
+             return sub;
+         }
