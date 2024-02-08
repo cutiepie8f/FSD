49d74ddecba9b743acaaf68c25a0a12702f8561a
cutiepie8f <135813683+cutiepie8f@users.noreply.github.com>
2024-02-08 21:41:17 +0530

    commit

--- Assignments/Assignment3/Question-2/2.js
+++ Assignments/Assignment3/Question-2/2.js
@@ -1,68 +1,45 @@
-class OrderDetails{
+orderData = {  
+    'Below 500': 20,  
+    '500-1000': 29,  
+    '1000-1500': 30,  
+    '1500-2000': 44,  
+    'Above 2000': 76  
+    }; 
     
-    orderData =[ {id:1,order:'Below 500',Totalorder: 20,restaurant: "Punjabi Tadka"},
-                 {id:2,order:'500-1000',Totalorder: 29,restaurant: "Punjabi Tadka"},
-                 {id:3,order:'1000-1500',Totalorder: 30,restaurant: "Punjabi Tadka"},
-                 {id:4,order:'1500-2000',Totalorder: 44,restaurant: "Punjabi Tadka"},
-                 {id:5,order:'Above 2000',Totalorder: 76,restaurant: "Punjabi Tadka"}
-  ];
-            
-  totalOrderPlaced()
-  {
-   var sum = 0;
-    this.orderData.forEach(element => {
-        sum += element.Totalorder;
-    });
-    return sum;
-  }
 
-  totalOrderProprtion =()=>
-  { 
-//     var count = 0;
-//    this.orderData.forEach(element => {
-//       count++;
-  // });
-  var arr = this.orderData.map(a=>a.Totalorder);
- // console.log(arr);
-  return arr.length;
-  // return count;
-      
-  }
+     // part-a       
+    var a = Object.values(orderData);
+    var sum = a.reduce((a,b) => a+b);
 
-  proportionPercentage(total)
-  {
-  for(var i=0;i<this.orderData.length;i++)
-    {
-        this.orderData[i].order_percentage = ((this.orderData[i].Totalorder*100)/total).toFixed(2);
-       delete this.orderData[i].Totalorder;
-      
-    }
-    return this.orderData;
-  }
-}
+    //part-b
+    var b = Object.keys(orderData);
+    var count = b.length;
 
-var OrderDetailsObject = new OrderDetails();
 
 function firstOperation()
 {
-   
-    var output1 = OrderDetailsObject.totalOrderPlaced();
     document.getElementById("result-text").innerHTML = "Total number of orders placed = ";
-    document.getElementById("result1").innerHTML =  output1;
+    document.getElementById("result1").innerHTML =  sum;
 }
 function secondOperation()
 {
-   
-    var output2 = OrderDetailsObject.totalOrderProprtion();
      document.getElementById("result-text2").innerHTML = "Total number of order proportions = ";
-     document.getElementById("result2").innerHTML =  output2;
+     document.getElementById("result2").innerHTML =  count;
 }
 
 function thirdOperation()
 {
-     var total_order = OrderDetailsObject.totalOrderPlaced();
-    var output3 = OrderDetailsObject.proportionPercentage(total_order);
-     console.log(output3);
+   var arr = []
+    b.map((items, index)=>{
+      var result = {
+                   'id':index+1,
+                   'order':items,
+                   'order percentage':((orderData[items]/sum)*100).toFixed(2),
+                  'restaurant': 'Punjabi Tadka' 
+      }
+       arr.push(result);
+    })
+    console.log(arr);
      document.getElementById("result-text3").innerHTML = "Please see the result on the console.....!";
     
 }
